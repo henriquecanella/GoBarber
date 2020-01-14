1e6c9d85893a9f394201dc2a23c23945d35b7f91
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-14 14:54:37 -0300

    Autenticação JWT adicionada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,10 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'bd03e4c7b5842f5222e2d33efa87887f',
+  expiresIn: '7d',
+};
