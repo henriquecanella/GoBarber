cf1905d983d574af33abd724d2dcb5c905fd99a2
Henrique Canella <henrique.mandelli@gmail.com>
2020-01-19 17:13:27 -0300

    Configurando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c9a9e3badd6cb5',
+    pass: '93b13eec58e1f3',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
