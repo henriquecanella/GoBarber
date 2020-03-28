ac71e0d60052d774540a174ee62201431a168f3b
Henrique Canella <henrique.mandelli@gmail.com>
2020-03-28 17:56:07 -0300

    conectando com reactotron e configurando redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
 import { NavigationContainer } from '@react-navigation/native';
+
+import '~/config/ReactotronConfig';
+
+import { store, persistor } from './store';
+
 import App from './App';
 
 export default function Index() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <App />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
     </NavigationContainer>
   );
 }
