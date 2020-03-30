50feddadb3ba0c960ab6f95763c2c97ab265d097
Henrique Canella <henrique.mandelli@gmail.com>
2020-03-30 20:18:47 -0300

    Modificaçoes para o projeto mobile

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -20,7 +20,7 @@ class AvailableController {
 
     const searchDate = Number(date);
 
-    const appointments = await Appointment.finddAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
@@ -43,11 +43,6 @@ class AvailableController {
       '17:00',
       '18:00',
       '19:00',
-      '20:00',
-      '21:00',
-      '22:00',
-      '23:00',
-      '24:00',
     ];
 
     const available = schedule.map(time => {
